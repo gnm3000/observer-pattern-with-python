2b95be33da9c29633aec4900287097d45f7ebdcb
German <gnm3000@gmail.com>
2022-02-12 01:31:06 -0300

    learning about Observer Pattern design with python

--- plan.py
+++ plan.py
@@ -1,8 +1,8 @@
-from lib.email import send_email
-from lib.db import create_user, find_user
-from lib.log import log
-from lib.slack import post_slack_message
-
+from email import send_email
+from db import create_user, find_user
+from log import log
+from slack import post_slack_message
+from event import post_event
 def upgrade_plan(email: str):
     # find the user
     user = find_user(email)
@@ -10,14 +10,5 @@ def upgrade_plan(email: str):
     # upgrade the plan
     user.plan = "paid"
 
-    # post a Slack message to sales department
-    post_slack_message("sales",
-        f"{user.name} has upgraded their plan.")
-
-    # send a thank you email
-    send_email(user.name, user.email,
-        "Thank you",
-        f"Thanks for upgrading, {user.name}! You're gonna love it. \nRegards, The DevNotes team")
-
-    # write server log
-    log(f"User with email address {user.email} has upgraded their plan")
+    # post event
+    post_event("user_upgrade_plan",user)
--- user.py
+++ user.py
@@ -1,24 +1,18 @@
-from lib.email import send_email
-from lib.db import create_user, find_user
-from lib.log import log
-from lib.slack import post_slack_message
-from lib.stringtools import get_random_string
+
+from db import create_user,find_user
+import random
+import string
+
+from event import post_event
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
 
 def register_new_user(name: str, password: str, email: str):
     # create an entry in the database
     user = create_user(name, password, email)
     
-    # post a Slack message to sales department
-    post_slack_message("sales",
-        f"{user.name} has registered with email address {user.email}. Please spam this person incessantly.")
-
-    # send a welcome email
-    send_email(user.name, user.email,
-        "Welcome",
-        f"Thanks for registering, {user.name}!\nRegards, The DevNotes team")
-
-    # write server log
-    log(f"User registered with email address {user.email}")
+    post_event("user_registered",user)
 
 def password_forgotten(email: str):
     # retrieve the user
@@ -27,9 +21,12 @@ def password_forgotten(email: str):
     # generate a password reset code
     user.reset_code = get_random_string(16)
 
-    # send a password reset message
-    send_email(user.name, user.email, "Reset your password",
-        f"To reset your password, use this very secure code: {user.reset_code}.\nRegards, The DevNotes team")
+    post_event("user_password_forgotten",user)
+def reset_password(code: str, email: str, password: str):
 
-    # write server log
-    log(f"User with email address {user.email} requested a password reset")
+    # retrieve the user
+    user = find_user(email)
+
+    # reset the password
+    user.reset_password(code, password)
+    
